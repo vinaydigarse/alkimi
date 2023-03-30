4b25a7be94cf9e5a392bf54114d11a35ac565551
vinaydigarse <vinaydigarse1@gmail.com>
2023-03-31 03:31:53 +0530

    final

--- my-app/src/pages/index.tsx
+++ my-app/src/pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '@/styles/Home.module.css'
-import Services from './Services'
+import Services from './services'
 import Faq from './faq'
 // const inter = Inter({ subsets: ['latin'] })
 
